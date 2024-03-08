143ddd2a2330228dc164957dcf788edb2410062f
Loïc PILLER <loic.piller@etu.univ-nantes.fr>
2024-03-08 17:07:54 +0100

    feat: catgories are init dynamically and filters works

--- FrontEnd/scripts/main.js
+++ FrontEnd/scripts/main.js
@@ -1,28 +1,60 @@
-const gallery = document.querySelector("#portfolio > .gallery")
+const APIURL = "http://localhost:5678/api";
+
+const DGALLERY = document.querySelector("#portfolio > .gallery")
+const FILTERS = document.querySelector("#portfolio > .filters-container")
 async function getJson(url) {
     const response = await fetch(url);
     return await response.json();
 }
 
-async function displayWorks(){
-    let works = await getJson("http://localhost:5678/api/works");
+async function initCategories(){
+    let categories = await getJson(APIURL + "/categories");
 
-    works.forEach((e) => {
-            const figure = document.createElement("figure");
+    let n = 1;
+    categories.forEach((category) => {
+        const filterButton = document.createElement("button");
+        filterButton.setAttribute('data-id', n);
+        filterButton.innerText = categories[n-1].name;
 
-            const img = document.createElement("img");
-            img.src = e.imageUrl;
-            img.alt = e.title;
+        FILTERS.appendChild(filterButton);
 
-            const caption = document.createElement("figcaption");
-            caption.innerText = e.title;
-
-            figure.appendChild(img);
-            figure.appendChild(caption)
-
-            gallery.appendChild(figure);
-        }
-    )
+        n += 1;
+    })
 }
 
-displayWorks().catch((error) => console.log(error));
+async function displayWorks(categoryId = 0){
+    let works = await getJson(APIURL + "/works");
+
+    if (categoryId !== 0) {
+        works = works.filter((work) => work.categoryId === categoryId);
+    }
+
+    DGALLERY.innerHTML = '';
+    works.forEach((work) => {
+        const figure = document.createElement("figure");
+
+        const img = document.createElement("img");
+        img.src = work.imageUrl;
+        img.alt = work.title;
+
+        const caption = document.createElement("figcaption");
+        caption.innerText = work.title;
+
+        figure.appendChild(img);
+        figure.appendChild(caption)
+
+        DGALLERY.appendChild(figure);
+    })
+}
+
+function initPage(){
+    FILTERS.addEventListener('click', function (e){
+        let categoryId = e.target.dataset.id;
+        if (categoryId === undefined) return;
+        displayWorks(parseInt(categoryId));
+    })
+}
+
+displayWorks();
+initCategories();
+initPage()
